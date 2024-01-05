2b29b9068471a7a2242d1fbcbe1f94ebab7a4ffa
CS-Edwards <candaces.edwards@gmail.com>
2024-01-04 19:36:18 -1000

    updated yaml/toml for type checking

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -19,4 +19,4 @@ def test_calc_extract():
 def test_calc_extract_parameterized(input_value, expected_value):
     """Test that rescale works correctly for multiple cases."""
     calc_value = calc_extract(input_value)
-    np.testing.assert_equals(calc_value, expected_value)
+    np.testing.assert_equal(calc_value, expected_value)
